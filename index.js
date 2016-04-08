1f53473d81a8d8d928343a1300f4985ab3cc8c1b
zonika <zchodosh@gmail.com>
2016-04-08 16:53:41 -0400

    add a test

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = leftSad;
-
 function leftSad(str, len) {
   str = String(str);
-  var i = 0;
+  var i = -1;
   while (++i < len) {
     str = "sad" + str;
   }
+
   return str;
 }
---
+++ test.js
@@ -0,0 +1,7 @@
+var leftSad = require('./');
+var test = require('tape');
+
+test('left sad', function(assert) {
+  assert.plan(1);
+  assert.strictEqual(leftSad("zoe", 2), "sadsadzoe");
+});
